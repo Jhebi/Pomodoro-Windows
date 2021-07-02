9121558b1aef6781f4547cd732c74823d0ea87de
Jhebi <53257356+Jhebi@users.noreply.github.com>
2021-07-02 20:57:37 +0800

    Added functionalities

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,14 +33,24 @@ namespace Pomodoro_Windows
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.Startbtn = new System.Windows.Forms.Button();
+            this.Quitbtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Quitbtn2 = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Backbtn = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 73);
+            this.label1.Location = new System.Drawing.Point(54, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
@@ -49,7 +59,7 @@ namespace Pomodoro_Windows
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 99);
+            this.label2.Location = new System.Drawing.Point(57, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 1;
@@ -57,33 +67,161 @@ namespace Pomodoro_Windows
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(146, 73);
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 40);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
             this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(146, 100);
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(152, 67);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(41, 20);
             this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Startbtn
+            // 
+            this.Startbtn.Location = new System.Drawing.Point(37, 119);
+            this.Startbtn.Name = "Startbtn";
+            this.Startbtn.Size = new System.Drawing.Size(75, 23);
+            this.Startbtn.TabIndex = 4;
+            this.Startbtn.Text = "Start";
+            this.Startbtn.UseVisualStyleBackColor = true;
+            this.Startbtn.Click += new System.EventHandler(this.Startbtn_Click);
+            // 
+            // Quitbtn
+            // 
+            this.Quitbtn.Location = new System.Drawing.Point(135, 119);
+            this.Quitbtn.Name = "Quitbtn";
+            this.Quitbtn.Size = new System.Drawing.Size(75, 23);
+            this.Quitbtn.TabIndex = 5;
+            this.Quitbtn.Text = "Quit";
+            this.Quitbtn.UseVisualStyleBackColor = true;
+            this.Quitbtn.Click += new System.EventHandler(this.Quitbtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.Quitbtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Startbtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Location = new System.Drawing.Point(43, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 177);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::Pomodoro_Windows.Properties.Resources.Red_Tomato;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.Quitbtn2);
+            this.panel2.Controls.Add(this.Pause);
+            this.panel2.Controls.Add(this.Backbtn);
+            this.panel2.Controls.Add(this.Timer);
+            this.panel2.Location = new System.Drawing.Point(374, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(273, 177);
+            this.panel2.TabIndex = 7;
+            // 
+            // Quitbtn2
+            // 
+            this.Quitbtn2.Location = new System.Drawing.Point(169, 119);
+            this.Quitbtn2.Name = "Quitbtn2";
+            this.Quitbtn2.Size = new System.Drawing.Size(58, 23);
+            this.Quitbtn2.TabIndex = 3;
+            this.Quitbtn2.Text = "Quit";
+            this.Quitbtn2.UseVisualStyleBackColor = true;
+            this.Quitbtn2.Click += new System.EventHandler(this.Quitbtn2_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(105, 119);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(58, 23);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "Start";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Backbtn
+            // 
+            this.Backbtn.Location = new System.Drawing.Point(40, 119);
+            this.Backbtn.Name = "Backbtn";
+            this.Backbtn.Size = new System.Drawing.Size(59, 23);
+            this.Backbtn.TabIndex = 1;
+            this.Backbtn.Text = "Back";
+            this.Backbtn.UseVisualStyleBackColor = true;
+            this.Backbtn.Click += new System.EventHandler(this.Backbtn_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.Location = new System.Drawing.Point(100, 80);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(71, 25);
+            this.Timer.TabIndex = 0;
+            this.Timer.Text = "00:00";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -93,6 +231,14 @@ namespace Pomodoro_Windows
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button Startbtn;
+        private System.Windows.Forms.Button Quitbtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Quitbtn2;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Backbtn;
+        private System.Windows.Forms.Label Timer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Pomodoro_Windows
@@ -16,5 +17,78 @@ namespace Pomodoro_Windows
         {
             InitializeComponent();
         }
+        System.Timers.Timer time;
+        int min, sec, wmin, rmin, lrmin;
+        
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            // if button starts or pauses depending on the text on the button
+            Button btn = sender as Button;
+            string p = btn.Text;
+            if (p == "Start")
+            {
+                time.Start();
+                Pause.Text = "Pause";
+            }
+
+            if (p == "Pause")
+            {
+                time.Stop();
+                Pause.Text = "Start";
+            }
+        }
+
+        private void Backbtn_Click(object sender, EventArgs e)
+        {
+            //shows panel 1 and hides panel 2
+            panel2.Hide();
+            panel1.Show();
+        }
+
+        private void Quitbtn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //setup timer
+            time = new System.Timers.Timer();
+            time.Interval = 1000; //1 second interval
+            time.Elapsed += OnTimeEvent;
+            //hides panel 2
+            panel2.Hide();
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            sec += 1; //counting seconds
+            Invoke(new Action(() =>
+                {
+                    if (sec == 60)
+                    {
+                        sec = 0;
+                        min += 1;
+                    }
+                    Timer.Text = string.Format("{0}:{1}", min.ToString().PadLeft(2, '0'), sec.ToString().PadLeft(2, '0'));
+                }));
+            
+        }
+
+        private void Startbtn_Click(object sender, EventArgs e)
+        {
+            //shows panel 2 and hides panel 1
+            panel1.Hide();
+            panel2.Show();
+
+        }
+
+        private void Quitbtn_Click(object sender, EventArgs e)
+        {
+            //stops the timer
+            time.Stop();
+        }
+
+       
     }
 }
